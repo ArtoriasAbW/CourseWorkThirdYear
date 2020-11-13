66b78e37abd2750ab5dd7a0d107c7a72deb82909
Atic_an <s02180046@gse.cs.msu.ru>
2020-11-13 16:21:05 +0300

    add black list for trace modules

--- DrTrace/modules_info.cpp
+++ DrTrace/modules_info.cpp
@@ -1,16 +1,8 @@
 #include "modules_info.hpp"
 
-/*
-typedef void(*ml_ptr)(void *drcontext, const module_data_t *info, bool loaded);
-typedef void(*ml_ptr_obj)(void *obj, ml_ptr ptr);
-typedef void(*mul_ptr)(void *drcontext, const module_data_t *info);
-*/
+static modules_info *mi_self = NULL;
 
-static modules_info *mi_self = NULL;// TODO: remove static (if it possible)
-
-//static void module_load_event(void *drcontext, const module_data_t *info, bool loaded);
-//static void module_unload_event(void *drcontext, const module_data_t *info);
-
+#pragma region check module info
 inline bool modules_info::check_ptr_in_module(app_pc ptr, size_t index)
 {
     return modules[index].id && modules[index].start <= ptr && ptr < modules[index].end && dr_module_contains_addr(modules[index].m_data, ptr);
@@ -29,17 +21,24 @@ app_pc modules_info::get_module_start(size_t index)
 {
     return modules[index].start;
 }
+#pragma endregion
 
+#pragma region Static Function : module load/unload event
 void
 modules_info::module_load_event(void *drcontext, const module_data_t *info, bool loaded)
 {
+
     static uint module_id = 0;
     module_id++;
 
     mi_self->modules.push_back(__module_info(module_id, info->start, info->end, dr_copy_module_data(info)));
 
+    bool not_trace = mi_self->not_trace_rules_on(module_id);//but we can turn on not tracing rule after load event
+
     dr_fprintf(mi_self->module_info_file, "[id = %04d]: [name = \"%s\"     path = \"%s\"]\n", module_id, dr_module_preferred_name(info), info->full_path);
     dr_fprintf(mi_self->module_info_file, "             [start = [%p]     end = [%p]]\n", info->start, info->end);
+    if(not_trace)
+    dr_fprintf(mi_self->module_info_file, "             [not initially traced]\n");
 }
 
 void
@@ -59,8 +58,11 @@ modules_info::module_unload_event(void *drcontext, const module_data_t *info)
         }
     }
 }
+#pragma endregion
 
-modules_info::modules_info(const char *file_name) : modules(), valid(true)
+#pragma region Constructor
+
+modules_info::modules_info(const char *file_name) : modules(), trace_blacklist(), valid(true), rules_not_trace_by_name(), rules_not_trace_by_path()
 {   
     if (mi_self != NULL) {
         dr_fprintf(STDERR, "TODO: remove static (if it possible)\n");
@@ -89,6 +91,9 @@ modules_info::modules_info(const char *file_name) : modules(), valid(true)
 
 }
 
+#pragma endregion
+
+#pragma region operator(s)
 modules_info &
 modules_info::operator= (modules_info &&other) noexcept
 {
@@ -99,11 +104,18 @@ modules_info::operator= (modules_info &&other) noexcept
     this->valid = other.valid;
     this->modules = std::move(other.modules);
     this->module_info_file = std::move(other.module_info_file);
+    
+    this->trace_blacklist = std::move(other.trace_blacklist);
+    this->rules_not_trace_by_name = std::move(other.rules_not_trace_by_name);
+    this->rules_not_trace_by_path = std::move(other.rules_not_trace_by_path);
 
     other.valid = false;
     mi_self = this;
     return *this;
 }
+#pragma endregion
+
+#pragma region Free
 
 bool modules_info::mi_free()
 {
@@ -116,7 +128,7 @@ bool modules_info::mi_free()
             dr_free_module_data(modules[i].m_data);
         }
     }
-
+   
     dr_close_file(module_info_file);
 
     bool ret = drmgr_unregister_module_load_event(module_load_event);
@@ -138,3 +150,135 @@ modules_info::~modules_info()
         dr_fprintf(STDERR, "not all moudle event handlers were unregistered\n");
     }
 }
+
+#pragma endregion
+
+
+//+++ out from this file:
+
+//TODO:not only eng chars.  (other language?)
+std::string str_to_lowercase(char *str)
+{
+    size_t len = std::strlen(str);
+    std::string ret;
+    ret.reserve(len);
+
+    for (int i = 0; i < len; i++) {
+        ret += std::tolower(str[i]);
+    }
+    
+    return ret;
+}
+
+std::string str_to_lowercase(std::string str)
+{
+    size_t len = str.length();
+    std::string ret;
+    ret.reserve(len);
+
+    for (int i = 0; i < len; i++) {
+        ret += std::tolower(str[i]);
+    }
+
+    return ret;
+}
+//--- out from this file:
+
+
+#pragma region add in trace blacklist
+void modules_info::module_add_not_trace_by_id(size_t id)
+{trace_blacklist.insert(id);}
+
+size_t modules_info::module_add_not_trace_by_name(const std::string &name, StringWayMatching way_matching = StringWayMatching::equal)
+{
+    size_t ret = 0;
+    not_trace_rule rule = {name, str_to_lowercase(name), way_matching};
+    rules_not_trace_by_name.push_back(rule);
+
+    size_t m_amount = modules.size();
+    for (int ind = 0; ind < m_amount; ind++) {
+        std::string lowercase_str {};
+        if (modules[ind].id == 0 || (modules[ind].m_data == NULL))continue;
+        ret += try_one_rule(rule, ind, dr_module_preferred_name(modules[ind].m_data), lowercase_str);
+    }
+
+    return ret;
+}
+
+size_t modules_info::module_add_not_trace_by_path(const std::string &path, StringWayMatching way_matching = StringWayMatching::equal)
+{
+    size_t ret = 0;
+    not_trace_rule rule = {path, str_to_lowercase(path), way_matching};
+    rules_not_trace_by_path.push_back(rule);
+    size_t m_amount = modules.size();
+    for (int ind = 0; ind < m_amount; ind++) {
+        std::string lowercase_str{};
+        if (modules[ind].id == 0 || (modules[ind].m_data == NULL))continue;
+        ret += try_one_rule(rule, ind, modules[ind].m_data->full_path, lowercase_str);
+    }
+    return ret;
+}
+
+bool modules_info::try_one_rule(const not_trace_rule &rule, size_t module_info_id, const char *str, std::string &lowercase_str, bool with_insert)
+{
+    size_t ind = module_info_id;
+    if (!ind || !modules[ind].id)return false;
+
+    switch (rule.way_matching) {
+    case StringWayMatching::equal:
+        if (!(rule.str == str)) return false;
+        break;
+
+    case StringWayMatching::equal_case_insensitive:
+        if (lowercase_str.empty())lowercase_str = str_to_lowercase(str);
+        if (!(rule.lowercase_str == lowercase_str)) return false;
+        break;
+
+    case StringWayMatching::contain:
+        if (rule.str.find(str) == std::string::npos) return false;
+        break;
+
+    case StringWayMatching::contain_case_insensitive:
+        if (lowercase_str.empty())lowercase_str = str_to_lowercase(str);
+        if (lowercase_str.find(rule.lowercase_str) == std::string::npos) return false;
+        break;
+    }
+    if (with_insert)trace_blacklist.insert(ind);
+    return true;
+}
+
+bool modules_info::try_rules(const std::vector<not_trace_rule> &rules_not_trace, size_t module_info_id, const char *str, bool with_insert)
+{
+    size_t ind = module_info_id;
+    if (modules[ind].id == 0)return false;
+
+    std::string lowercase_str {};
+
+    for (auto &rule : rules_not_trace) {
+        if (try_one_rule(rule, module_info_id, str, lowercase_str, with_insert))return true;
+    }
+
+    return false;
+}
+
+bool modules_info::not_trace_rules_on(size_t module_info_id)
+{
+    size_t ind = module_info_id;
+    if (modules[ind].id == 0 || (modules[ind].m_data == NULL))return false;
+
+    const char *prefer_name = dr_module_preferred_name(modules[ind].m_data);
+    const char *path = modules[ind].m_data->full_path;
+
+    if (try_rules(rules_not_trace_by_name, module_info_id, prefer_name))return true;
+    if (try_rules(rules_not_trace_by_path, module_info_id, path))return true;
+
+    return false;
+}
+
+bool modules_info::need_to_trace(size_t module_info_id)
+{
+    return !trace_blacklist.count(module_info_id);
+}
+#pragma endregion
+
+
--- DrTrace/modules_info.hpp
+++ DrTrace/modules_info.hpp
@@ -2,8 +2,11 @@
 
 //cpp:
 #include <vector>
-//#include <map>
+#include <string>
+#include <set>
 
+//#include <optional>
+//MAYBE: in try_on_rule std::string &lowercase_str -> std::optional<std::string&> lowercase_optional;
 
 typedef struct __module_info
 {
@@ -15,24 +18,62 @@ typedef struct __module_info
     __module_info(uint _id, app_pc _start, app_pc _end, module_data_t *ptr) : id(_id), start(_start), end(_end), m_data(ptr) {}
 }__module_info;
 
+
+enum class StringWayMatching
+{
+    equal,
+    equal_case_insensitive,
+    contain,
+    contain_case_insensitive,
+};
+
 struct modules_info
 {
 private:
     file_t module_info_file;
     std::vector<__module_info> modules;//MAYBE:<module_info *>   |   now, while we have only 4 fields so ptr is needless  
                                        //MAYBE: Dictionary<app_pc start, module_info> and find addr between two keys: (key_1 <= pc < key_2) & (pc<=m[key_1].end) => pc in module
+    
+    /// after  "module_info m_info = m_info_2;"  m_info_2 become invalid;
+    /// not valid  =>  don't close module_info_file; don't dr_free_module_data; etc.
     bool valid;
 public:
-    modules_info() : module_info_file(0), valid(false) {};
+    modules_info() : module_info_file(0), valid(false), trace_blacklist(), rules_not_trace_by_name(), rules_not_trace_by_path() {};
     modules_info(const char *file_name);
     modules_info &operator= (modules_info &&other) noexcept;
-
+//free:
     ~modules_info();
     bool mi_free();
-    
+//info about module:
+    /// <summary>return ptr on start module by id</summary>
     app_pc get_module_start(size_t index);
     bool check_ptr_in_module(app_pc ptr, size_t index);
+    /// <summary>return module id != 0 if ptr in module; otherwise return 0</summary>
     size_t get_module_id(app_pc ptr);
+
+
+private: 
+    std::set<size_t> trace_blacklist;
+
+    struct not_trace_rule
+    {
+        std::string str;
+        std::string lowercase_str;
+        StringWayMatching way_matching;
+    };
+    std::vector<not_trace_rule> rules_not_trace_by_name;
+    std::vector<not_trace_rule> rules_not_trace_by_path;
+public: 
+    void module_add_not_trace_by_id(size_t id);
+    size_t module_add_not_trace_by_name(const std::string &name, StringWayMatching way_matching);
+    size_t module_add_not_trace_by_path(const std::string &path, StringWayMatching way_matching);
+private:
+    bool not_trace_rules_on(size_t module_info_id);
+    bool try_one_rule(const not_trace_rule &rule, size_t module_info_id, const char *str, std::string &lowercase_str, bool with_insert = true);
+    bool try_rules(const std::vector<not_trace_rule> &rules_not_trace, size_t module_info_id, const char *str, bool with_insert = true);
+public:
+    bool need_to_trace(size_t module_info_id);
+
 
 private:
     //TODO:NEXT 2 LINES:I don't know how to make it wo static
